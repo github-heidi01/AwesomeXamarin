1558a304c3fafb500cdb3acf35a76a01639a5692
gitstrato <gitstrato@4by.eu>
2023-07-06 12:30:55 +0200

    initial after tutorial

---
+++ AwesomeXamarin.UWP/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace AwesomeXamarin.UWP
+{
+    public sealed partial class MainPage
+    {
+        private int count = 0;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new AwesomeXamarin.App());
+        }
+
+        
+        private void Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            count++;
+            CountHere.Text = $"You clicked {count} times.";
+        }
+    }
+}
---
+++ AwesomeXamarin/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AwesomeXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        private int count = 0;
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            CountHere.Text = $"You clicked {count} times.";
+        }
+    }
+}
